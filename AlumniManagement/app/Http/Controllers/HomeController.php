77f4034e78fa241aaaa63bccf704755dab391e30
tuan_tt <tuan_tt@septeni-technology.jp>
2018-10-04 22:33:28 +0700

    chương trình quản lý sinh viên

--- AlumniManagement/app/Http/Controllers/HomeController.php
+++ AlumniManagement/app/Http/Controllers/HomeController.php
@@ -17,14 +17,15 @@ use Illuminate\Support\Facades\DB;
 class HomeController extends Controller {
     //method demo
     public function show(){
-        //thao tac database su dung DB facade
+        //C1: thao tac database su dung DB facade, query, lấy ra danh sách sinh viên và tên tỉnh, huyện của sv đó
 //        $alumni = DB::table('alumni')->join('courses', 'alumni.course_id', '=', 'courses.id')
 //            ->join('provinces', 'alumni.province_id', '=', 'provinces.id')
 //            ->join('districts', 'alumni.district_id', '=', 'districts.id')
 //            ->select('alumni.name', 'alumni.birthday', 'alumni.phone', 'alumni.mail','courses.name as course_name',
 //                'provinces.name as province_name', 'districts.name as district_name')->get();
-        //thao tac database su dung model
-
+        //---------------------------------------------------------------------------
+        //C2: thao tac database su dung Eloquent model, cũng lấy ra ds sv và tên tỉnh huyện của sv đó
+        //thao tac với db nên sử dụng Eloquent model, sẽ nhanh và gọn gàng hơn rất nhiều
         $alumni = Alumni::all();                        //lay ra danh sach cuu sinh vien
         return view('home', ['alumni'=>$alumni]); //truyen danh sach vao blade template
     }
